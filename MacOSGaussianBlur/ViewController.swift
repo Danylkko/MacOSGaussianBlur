62c6f57959aaf085ecd1ddbc9244992f086ef8a1
danylo <ltvnchkd@gmail.com>
2022-06-11 18:15:15 +0200

    Initial Commit

---
+++ MacOSGaussianBlur/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MacOSGaussianBlur
+//
+//  Created by Danylo Litvinchuk on 11.06.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
